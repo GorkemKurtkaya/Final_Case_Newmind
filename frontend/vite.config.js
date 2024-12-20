ab4c504d96e50af1c4307b399a61650cc9d738a7
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-12-20 16:45:23 +0300

    Docker Orkestrasyonu +

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Dış erişime izin ver
+    port: 5173,      // Docker içindeki portu sabitle
+    strictPort: true // Port çakışması varsa hata ver
+  },
+
 })
